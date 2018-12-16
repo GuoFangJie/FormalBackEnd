f68a83c0c40f325c788c8699ba4f79d6541df1c7
RenJianpeng <qwe1138318433@qq.com>
2018-12-16 20:14:05 +0800

    不知道加了啥为了pull

--- bird/gugu-admin/src/main/java/com/gugu/guguadmin/GuguAdminApplication.java
+++ bird/gugu-admin/src/main/java/com/gugu/guguadmin/GuguAdminApplication.java
@@ -2,8 +2,10 @@ package com.gugu.guguadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.gugu.gugumodel", "com.gugu.guguadmin"})
 public class GuguAdminApplication {
 
     public static void main(String[] args) {
--- bird/gugu-admin/src/main/java/com/gugu/guguadmin/service/StudentServiceImpl.java
+++ bird/gugu-admin/src/main/java/com/gugu/guguadmin/service/StudentServiceImpl.java
@@ -2,6 +2,7 @@ package com.gugu.guguadmin.service;
 
 import com.gugu.guguadmin.service.StudentService;
 import com.gugu.gugumodel.dao.StudentDao;
+import com.gugu.gugumodel.dao.StudentDaoImpl;
 import com.gugu.gugumodel.pojo.vo.StudentBasicInforVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,7 +16,7 @@ import java.util.List;
 @Service
 public class StudentServiceImpl implements StudentService {
     @Autowired
-    StudentDao studentDao;
+    StudentDaoImpl studentDao;
     @Override
     public void deleteStudentById(long id){
         studentDao.deleteStudentById(id);
